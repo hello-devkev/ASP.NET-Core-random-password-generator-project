95aa8e8c952dd7d7bfcfcbaab28246cd2755c449
devkev <124761822+hello-devkev@users.noreply.github.com>
2023-08-03 23:42:32 -0700

    Add files via upload
    Initial Commit

---
+++ Pages/Controllers/PasswordGeneratorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordGeneratorProject;
+
+namespace YourWebAppNamespace.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PasswordGeneratorController : ControllerBase
+    {
+        [HttpGet("GeneratePassword")]
+        public IActionResult GeneratePassword(int length, bool includeNumbers, bool includeSpecialChars)
+        {
+            string generatedPassword = PasswordGenerator.GeneratePassword(length, includeNumbers, includeSpecialChars);
+            return Ok(new { password = generatedPassword });
+        }
+    }
+}
---
+++ Pages/PasswordGenerator.cs
@@ -0,0 +1,35 @@
+﻿// PasswordGenerator.cs
+using System;
+using System.Text;
+
+namespace PasswordGeneratorProject
+{
+    public static class PasswordGenerator
+    {
+        private static readonly string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private static readonly string Numbers = "0123456789";
+        private static readonly string SpecialCharacters = "!@#$%^&*()_-+=[]{}|:;<>,.?/";
+
+        public static string GeneratePassword(int length, bool includeNumbers = true, bool includeSpecialChars = true)
+        {
+            StringBuilder result = new StringBuilder(length);
+            string validChars = Alphabet;
+
+            if (includeNumbers)
+                validChars += Numbers;
+            if (includeSpecialChars)
+                validChars += SpecialCharacters;
+
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(validChars.Length);
+                result.Append(validChars[index]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
+
